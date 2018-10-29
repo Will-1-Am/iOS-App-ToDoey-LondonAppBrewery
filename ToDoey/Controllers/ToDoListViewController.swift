2869b7314d79e1036cd0b964c153ad7cc8d2ea7e
William Spanfelner <wil-1-am@outlook.com>
2018-10-29 12:46:03 +0200

    Convert to MVC design pattern

--- ToDoey/Controllers/ToDoListViewController.swift
+++ ToDoey/Controllers/ToDoListViewController.swift
@@ -12,15 +12,31 @@ class ToDoListViewController: UITableViewController {
 
 //    let itemArray = ["Bread", "Milk", "Cheese", "Eggs"]  /* As we cannot change the value of a constant, a new array must be declared as a variable.*/
     
-    var itemArray = ["Bread", "Milk", "Cheese", "Eggs"]
+//    var itemArray = ["Bread", "Milk", "Cheese", "Eggs"]
+  
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+//        newItem.done = true  /* used for debugging */
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
     
@@ -35,9 +51,27 @@ class ToDoListViewController: UITableViewController {
     
     //TODO: Declare cellForRowAtIndexPath here:
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        print("cellForRowAtIndexPath called")
+        
+//        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator ==>
+        //value = condition ? valueIfTrue : valueIfFalse
+        cell.accessoryType = item.done ? .checkmark : .none  /* This line does the same thing as the if block below
+                                                                and reads, "Set the cell's accessory type, depending upon whether item.done is true.  If true, then set the checkmark otherwise set item.done to .none */
+        
+        if item.done == true {
+            cell.accessoryType = .checkmark
+        }else {
+            cell.accessoryType = .none
+        }
+        
         
         return cell
     }
@@ -48,14 +82,25 @@ class ToDoListViewController: UITableViewController {
 //        print(itemArray[indexPath.row])
         
 //        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark /* When a list item is tapped a checkmark appears at the right of the listing.  The checkmark remains whether we tap the item again or not - we want it to be unticked if tapped again */
+        
+        itemArray[indexPath.row].done.toggle() /* this is the same as the one line below */
+        
+//        itemArray[indexPath.row].done = !itemArray[indexPath.row].done   /* this is the same as the three if block below */
+//
+//        if itemArray[indexPath.row].done == false {
+//            itemArray[indexPath.row].done = true
+//        }else {
+//            itemArray[indexPath.row].done = false
+//        }
+        
         /* The conditional below takes care of turning the checkmark on or off given the correct tapping */
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }else{
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
         
-        
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true) /* This allows the selected row to return to its previous visual state instead of remaining grey after a row is tapped */
     }
@@ -73,7 +118,11 @@ class ToDoListViewController: UITableViewController {
             //what will happen when the user click the Add Item button on our UIAlert
 //            print(textFeild.text) /* The info in this print statement is what we are after, and will need to be appended to the array*/
             
-            self.itemArray.append(textFeild.text!)
+            let newItem = Item()
+            newItem.title = textFeild.text!
+            
+//            self.itemArray.append(textFeild.text!)
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,24 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by William Spanfelner on 29/10/2018.
+//  Copyright © 2018 William Spanfelner. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    // MARK: Properties
+    
+    var title : String = "" /* where titles of the to dos will be saved */
+    
+    var done : Bool = false /* whether the item was done or not will be saved */
+    
+    
+}
+
+
+
+
